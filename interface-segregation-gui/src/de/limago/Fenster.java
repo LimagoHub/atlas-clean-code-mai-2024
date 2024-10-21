95b248d500bc904054da8917d79a5b0d2697ca83
Jo Wagner <jowagner@limago.de>
2024-10-21 14:32:23 +0200

    NimGame erste Fassung

--- interface-segregation-gui/src/de/limago/Fenster.java
+++ interface-segregation-gui/src/de/limago/Fenster.java
@@ -4,14 +4,15 @@ import java.awt.*;
 import java.awt.event.*;
 
 
-public class Fenster extends Frame {
+public class Fenster extends Frame{
 
 
     public Fenster()  {
 
         setSize(300, 300);
         Button button = new Button("Drück mich");
-
+        button.addActionListener(e->ausgabe());
+        addWindowListener(new MyWindowListerner());
         add(button);
 
     }
@@ -23,5 +24,25 @@ public class Fenster extends Frame {
         new Fenster().setVisible(true);
     }
 
+    private void ausgabe() {
+        System.out.println("Button wurde gedrueckt");
+    }
 
+    private void beenden() {
+        // Daten speichern
+        dispose();
+    }
+
+   /*private class ButtonListener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            ausgabe();
+        }
+   }*/
+
+   class MyWindowListerner extends WindowAdapter {
+       @Override
+       public void windowClosing(final WindowEvent e) {
+           beenden();
+       }
+   }
 }
